445704e3df8fac3f1f131adef598e582b9018cf1
Tobias Kortkamp <tobik@FreeBSD.org>
2019-01-31 08:56:59 +0100

    Run tests again with the expected results as input
    To make sure the output does not randomly change.
    
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- run-tests.sh
+++ run-tests.sh
@@ -7,10 +7,16 @@ cd tests
 : ${TESTS:=*.in}
 for test in ${TESTS}; do
 	t=${test%*.in}
-	${PORTFMT} < ${test} | diff -L ${t}.expected -L ${t}.actual \
-		-u ${t}.expected -
+	${PORTFMT} < ${t}.in > ${t}.actual
+	diff -L ${t}.expected -L ${t}.actual -u ${t}.expected ${t}.actual
+	if [ $? -ne 0 ]; then
+		status=1
+	fi
+	${PORTFMT} < ${t}.expected > ${t}.actual2
+	diff -L ${t}.expected -L ${t}.actual -u ${t}.expected ${t}.actual2
 	if [ $? -ne 0 ]; then
 		status=1
 	fi
 done
+rm -f *.actual *.actual2
 exit ${status}
