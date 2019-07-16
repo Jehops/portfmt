dcbc3772d7cd66b8b1d885fa5ec49fb3ceaca795
Tobias Kortkamp <tobik@FreeBSD.org>
2019-07-16 11:18:29 +0200

    Fix segfault in is_include_bsd_port_mk()
    Token data can return NULL for CONDITIONAL_{END,START}, so check
    that we have a CONDITIONAL_TOKEN first.
    
    Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

--- rules.c
+++ rules.c
@@ -2165,7 +2165,8 @@ int
 is_include_bsd_port_mk(struct Token *t)
 {
 	struct Conditional *c = token_conditional(t);
-	return c && conditional_type(c) == COND_INCLUDE &&
+	return c && token_type(t) == CONDITIONAL_TOKEN &&
+		conditional_type(c) == COND_INCLUDE &&
 		(strcmp(token_data(t), "<bsd.port.options.mk>") == 0 ||
 		strcmp(token_data(t), "<bsd.port.pre.mk>") == 0 ||
 		strcmp(token_data(t), "<bsd.port.post.mk>") == 0 ||
