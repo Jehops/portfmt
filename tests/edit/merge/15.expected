Jehops/portfmt

commit 975530513eeeed71b27b36035beb32fb87ba9e3c

merge: Insert new variables in the right places
Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

edit_merge.c
@@ -50,6 +50,7 @@ struct MergeParameter {
 	struct Array *values;
 };
 
+static void append_new_variable(struct Parser *, struct Array *, struct Variable *, struct Range *);
 static struct Variable *has_variable(struct Array *, struct Variable *);
 static struct Array *extract_tokens(struct Parser *, struct Array *, enum ParserError *, const void *);
 static struct Array *insert_variable(struct Parser *, struct Array *, enum ParserError *, const void *);
@@ -122,26 +123,63 @@ assign_values(struct Parser *parser, struct Array *tokens, const struct MergePar
 	}
 }
 
+void
+append_new_variable(struct Parser *parser, struct Array *tokens, struct Variable *var, struct Range *lines) 
+{
+	struct Token *t = token_new2(VARIABLE_START, lines, NULL, var, NULL, NULL);
+	array_append(tokens, t);
+	parser_mark_edited(parser, t);
+	t = token_new2(VARIABLE_END, lines, NULL, var, NULL, NULL);
+	array_append(tokens, t);
+	parser_mark_edited(parser, t);
+}
+
 struct Array *
 insert_variable(struct Parser *parser, struct Array *ptokens, enum ParserError *error, const void *userdata)
 {
 	struct Variable *var = (struct Variable *)userdata;
 
 	ssize_t insert_after = -1;
+	for (size_t i = 0; i < array_len(ptokens); i++) {
+		struct Token *t = array_get(ptokens, i);
+		if (token_type(t) != VARIABLE_END) {
+			continue;
+		}
+
+		char *a = variable_name(token_variable(t));
+		char *b = variable_name(var);
+		int cmp = compare_order(&a, &b);
+		assert(cmp != 0);
+		if (cmp < 0) {
+			insert_after = i;
+		} else {
+			break;
+		}
+	}
 
 	if (insert_after == -1) {
-		struct Range lines = { 0, 1 };
-		struct Token *t = token_new2(VARIABLE_START, &lines, NULL, var, NULL, NULL);
-		array_append(ptokens, t);
-		parser_mark_edited(parser, t);
-		t = token_new2(VARIABLE_END, &lines, NULL, var, NULL, NULL);
-		array_append(ptokens, t);
-		parser_mark_edited(parser, t);
+		struct Range lines = {0, 1};
+		append_new_variable(parser, ptokens, var, &lines);
 		return NULL;
 	}
 
-	*error = PARSER_ERROR_EDIT_FAILED;
-	return NULL;
+	assert(insert_after > 0);
+
+	struct Array *tokens = array_new(sizeof(struct Token *));
+	int insert_next = 0;
+	for (size_t i = 0; i < array_len(ptokens); i++) {
+		struct Token *t = array_get(ptokens, i);
+		if (insert_next && token_type(t) != COMMENT) {
+			insert_next = 0;
+			append_new_variable(parser, tokens, var, token_lines(t));
+		} else if (token_type(t) == VARIABLE_END && ((size_t)insert_after) == i) {
+			insert_next = 1;
+		}
+
+		array_append(tokens, t);
+	}
+
+	return tokens;
 }
 
 struct Array *
@@ -221,6 +259,7 @@ edit_merge(struct Parser *parser, struct Array *ptokens, enum ParserError *error
 				if (*error != PARSER_ERROR_OK) {
 					goto cleanup;
 				}
+				parser_edit(parser, extract_tokens, &ptokens);
 			}
 			switch (variable_modifier(var)) {
 			case MODIFIER_APPEND:

tests/edit/merge/3.expected
@@ -1,0 +1,6 @@
+PORTNAME=	tokei
+
+USES=		gmake
+CARGO_CRATES=
+CARGO_USE_GITHUB=
+USERS=		_sndio

tests/edit/merge/3.in
@@ -1,0 +1,3 @@
+PORTNAME=	sndio
+
+USERS=		_sndio

tests/edit/merge/3.sh
@@ -1,0 +1,2 @@
+printf "PORTNAME=tokei\nUSES=gmake\nCARGO_CRATES=\nCARGO_USE_GITHUB=\n" | ${PORTEDIT} merge 3.in | \
+	diff -L 3.expected -L 3.actual -u 3.expected -

commit 41b18eadbbae30c2c0473875baf8d86fa4e43c07

merge: insert new variables in better places
Always do a complete search.

Regressed by 975530513eeeed71b27b36035beb32fb87ba9e3c

Signed-off-by: Tobias Kortkamp <tobik@FreeBSD.org>

edit_merge.c
@@ -183,8 +183,6 @@ insert_variable(struct Parser *parser, struct Array *ptokens, enum ParserError *
 		if (cmp < 0) {
 			block_before = variable_order_block(a);
 			insert_after = i;
-		} else {
-			break;
 		}
 	}
 

tests/edit/merge/15.expected
@@ -1,0 +1,7 @@
+USES=		autoreconf libtool pkgconfig
+
+USE_LDCONFIG=	yes
+
+USE_GITHUB=	yes
+GH_ACCOUNT=	foo
+GH_TAGNAME=	ababab

tests/edit/merge/15.in
@@ -1,0 +1,6 @@
+USES=		autoreconf libtool pkgconfig
+
+USE_LDCONFIG=	yes
+
+USE_GITHUB=	yes
+GH_ACCOUNT=	foo

tests/edit/merge/15.sh
@@ -1,0 +1,3 @@
+cat <<EOD | ${PORTEDIT} merge 15.in | diff -L 15.expected -L 15.actual -u 15.expected -
+GH_TAGNAME=	ababab
+EOD